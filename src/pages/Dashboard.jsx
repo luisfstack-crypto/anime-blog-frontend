4590c93854e3a5f4a8d8885454d937f067e8d2db
Luis Pech <luisfstack@gmail.com>
2026-03-22 18:34:32 -0600

    feat: setup basic routing and create initial page components

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">Student Dashboard</h1>
+      <p>Manage your posts here.</p>
+    </div>
+  )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="P-8">
+            <h1 className="text-2xl font-bold">Anime Blog Feed</h1>
+            <p>Latest anime posts will appear here.</p>
+        </div>
+    )
+}
